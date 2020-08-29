8696acd3306e326dbbf2600700a344ad1f479497
Mark <amwhow@gmail.com>
2020-08-29 12:38:26 +0000

    First commit - finished navbar but the javascript dropdown function

---
+++ src/navbar.js
@@ -0,0 +1,7 @@
+const navbarlinks = document.getElementsByClassName('navbarlinks')[0];
+
+document.getElementsByClassName('toggle_button').addEventListener("click", () => {
+    navbarlinks.classList.toggle('active')
+})
+
+
