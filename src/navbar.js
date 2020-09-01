0966912e242dc07aeba210dc48ed092d2a8c91b9
Mark <amwhow@gmail.com>
2020-09-01 05:26:26 +0000

    finished all elements for the home page, fixed the navbar dropdown and animated View Projects button issues

--- src/navbar.js
+++
@@ -1,7 +0,0 @@
-const navbarlinks = document.getElementsByClassName('navbarlinks')[0];
-
-document.getElementsByClassName('toggle_button').addEventListener("click", () => {
-    navbarlinks.classList.toggle('active')
-})
-
-
